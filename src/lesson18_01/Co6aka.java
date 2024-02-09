8430a924e7c8e09fd91116230837761b777e3ccc
Maksym <brute1988@gmail.com>
2024-02-09 14:59:21 +0100

    Employee

---
+++ src/lesson18_01/Co6aka.java
@@ -0,0 +1,82 @@
+/*
+ * created by Max$
+ */
+
+
+package lesson18_01;
+
+import javax.sound.midi.Soundbank;
+
+/*
+Также должен быть метод взять барьер. В параметрах метод принимает высоту барьера.
+ Если собака в состоянии преодолеть этот барьер - прыгает. Если не в состоянии, нужно проверить
+ помогут ли тренировки (будет ли данная собака в принципе в состоянии взять барьер).
+ Если да -> идем тренироваться -> берет барьер
+ */
+public class Co6aka {
+private String name;
+private double hochSpringen;
+
+    public Co6aka(String name, double hochSpringen) {
+        this.name = name;
+        this.hochSpringen = hochSpringen;
+    }
+
+    private void springCobaka(double hochSpringen){
+        System.out.println("Гав! Я Прыгаю! Прыжек на: " + hochSpringen);
+    }
+    private double trenningCo6aka(int countTrennig){
+        double maxSpringen = hochSpringen;
+        if (countTrennig <= 0){
+            System.out.println("Я не могу тренироваться вспять! Гав!");
+        }else {
+            for (int i = 0; i < countTrennig; i++) {
+                hochSpringen += 0.1;
+            }
+        }
+        if (maxSpringen*2 >= hochSpringen){
+            maxSpringen = hochSpringen;
+        }else {
+            hochSpringen = maxSpringen*2;
+        }
+
+        return hochSpringen;
+    }
+
+    private void barier(double hochBarier){
+        double maxSpring2 =  hochSpringen*2;
+        if (hochBarier <= hochSpringen){
+            System.out.println("ГАВ! Прыгаю!!!");
+        }else if(maxSpring2 > hochBarier){
+            for (int i = 1; hochSpringen < hochBarier; i++) {
+                trenningCo6aka(i);
+
+            }
+        System.out.println("Я натренировался и теперь прыгну на " + hochSpringen);
+        }else{
+            System.out.println("Это нереально! Ищи кенгуру!");
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHochSpringen() {
+        return hochSpringen;
+    }
+
+    public void setHochSpringen(double hochSpringen) {
+        this.hochSpringen = hochSpringen;
+    }
+
+    public static void main(String[] args) {
+        Co6aka dog = new Co6aka("Bobik", 1.1);
+        dog.barier(2);
+    }
+}
