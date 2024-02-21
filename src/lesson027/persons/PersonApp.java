cd1088d2b7b3acee02f63b52371b9803c116da00
Maksym <brute1988@gmail.com>
2024-02-21 23:47:03 +0100

    HW 27

---
+++ src/lesson027/persons/PersonApp.java
@@ -0,0 +1,13 @@
+/*
+ * created by max$
+ */
+
+
+package lesson027.persons;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person person = new Person("email", "password");
+        System.out.println(person);
+    }
+}
